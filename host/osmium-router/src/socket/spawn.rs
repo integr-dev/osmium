33fda58f392ef48d08c7d83f704719d36ef29af7
qweru <itsbookx@gmail.com>
2026-08-12 00:14:26 +0300

    chore(host): switch dockerimage to rust nightly

--- host/osmium-router/src/socket/spawn.rs
+++ host/osmium-router/src/socket/spawn.rs
@@ -1,5 +1,18 @@
-use tokio::net::TcpStream;
+use tokio_tungstenite::tungstenite::client::IntoClientRequest;
 
 pub async fn spawn_ws_client(addr: String, token: String) {
-    let stream = TcpStream::connect(addr);
+    let mut request = addr.into_client_request().expect("Invalid backend address");
+
+    request.headers_mut().insert(
+        "Authorization",
+        format!("Bearer {token}")
+            .parse()
+            .expect("Invalid token format"),
+    );
+
+    let (stream, _) = tokio_tungstenite::connect_async(request)
+        .await
+        .expect("Failed to locate backend");
+
+    println!("connected :3")
 }
